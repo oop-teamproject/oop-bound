93b49abd1f26771bde54fad9d5e3a52fea5756e9
whitw <shkim7876@naver.com>
2019-11-26 21:36:47 +0900

    image test

--- oop-bound/main.cpp
+++ oop-bound/main.cpp
@@ -1,8 +1,35 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
-	while (true);
+	// create the window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	sf::Texture texture;
+	if (!texture.loadFromFile("image.png")) {
+		//error..
+	}
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setTextureRect(sf::IntRect(0, 0, texture.getSize().x, texture.getSize().y));
+	sprite.setPosition(100, 100);
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+		window.draw(sprite);
+		// end the current frame
+		window.display();
+	}
+
 	return 0;
 }
